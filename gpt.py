efda5e2d449ad808a650b991d0097b3c5e630240
Roshan21424 <roshanraj21042004@gmail.com>
2025-12-09 21:37:19 +0530

    character tokenization

--- gpt.py
+++ gpt.py
@@ -12,3 +12,12 @@ chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print("unique chars in dataset: ",chars)
 print("vocab size: ",vocab_size)
+
+#character tokenization  
+from re import I
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos = {i:ch for i,ch in enumerate(chars)}
+encode =lambda s:[stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+print(encode('hii there'))
+print(decode(encode('hii there')))
